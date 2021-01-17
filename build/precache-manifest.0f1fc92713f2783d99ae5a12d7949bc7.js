71f6bcf0f36cdea8bbe5afcf09764a4d03b7bd2f
Lawrence Lin <lawrencelin101@gmail.com>
2021-01-17 02:49:40 -0500

    Update title in package.json

---
+++ build/precache-manifest.0f1fc92713f2783d99ae5a12d7949bc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3463ac5f1f7a56861b96146a4569f896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90638a9900985ab41780",
+    "url": "/static/css/2.1d850c3f.chunk.css"
+  },
+  {
+    "revision": "4b13119f6f4c3dc83c35",
+    "url": "/static/css/main.f35ff6cc.chunk.css"
+  },
+  {
+    "revision": "90638a9900985ab41780",
+    "url": "/static/js/2.cf64d391.chunk.js"
+  },
+  {
+    "revision": "26a6d6d7cfd8530a950e0a6fbb749684",
+    "url": "/static/js/2.cf64d391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b13119f6f4c3dc83c35",
+    "url": "/static/js/main.f31a79ec.chunk.js"
+  },
+  {
+    "revision": "e995c923f470a70e0847",
+    "url": "/static/js/runtime-main.a5483d26.js"
+  }
+]);
